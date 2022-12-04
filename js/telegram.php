46b358ad84e009b6fe0a5463903956aa05edb958
Petrov Bogdan <bogdan_emdetei_petrov@mail.ru>
2022-12-04 08:11:42 +0300

    правки

--- js/telegram.php
+++ js/telegram.php
@@ -1,29 +1,39 @@
 <?php
 
+/*получаем значения полей из формы*/
 $name = $_POST['user_name'];
 $number = $_POST['user_number'];
 $data = $_POST['user_data'];
 $time = $_POST['user_time'];
 $phone = $_POST['user_phone'];
-$token = "5951954877:AAEtAPBYQGAmmbfRom_S-vuyWNIKf8E2lv4";
-$chat_id = "-791524548";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Колличество гостей: ' => $number,
-  'Дата' => $data,
-  'Время:' => $time,
-  'Телефон:' => $phone
-);
 
-foreach($arr as $key => $value) {
-  $txt .= "<b>".$key."</b> ".$value."%0A";
-};
-
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
-if ($sendToTelegram && $sendToTelegram2) {
-  header('Location: thanks.html');
-} else {
-  echo "Error";
+/*функция для создания запроса на сервер Telegram */
+function parser($url){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    if($result == false){     
+      echo "Ошибка отправки запроса: " . curl_error($curl);
+      return false;
+    }
+    else{
+      return true;
+    }
 }
+
+/*собираем сообщение*/
+$message .= "Новое сообщение из формы";
+$message .= "Имя: ".$name;
+$message .= "колличество:".$number;
+$message .= "дата:".$data;
+$message .= "время:".$time;
+$message .= "телефон:".$phone;
+
+/*токен который выдаётся при регистрации бота */
+$token = "5951954877:AAEtAPBYQGAmmbfRom_S-vuyWNIKf8E2lv4";
+/*идентификатор группы*/
+$chat_id = "-791524548";
+/*делаем запрос и отправляем сообщение*/
+parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}");
 ?>
